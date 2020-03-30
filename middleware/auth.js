3746942658802e1133d4b33cfee57e42e95cf114
Elizabeth Schwab <elizabeth.schwab@grantstreet.com>
2020-03-30 09:42:46 -0400

    team check working

--- middleware/auth.js
+++ middleware/auth.js
@@ -46,7 +46,6 @@ module.exports = function authenticate(req, res, next) {
       }
 
       let userBody = JSON.parse(userResponse.body)
-
       let userTeams = userBody.teams
       let userRoleIds = userBody.roles
 
@@ -68,35 +67,21 @@ module.exports = function authenticate(req, res, next) {
           }
   
           let projectBody = JSON.parse(projectResponse.body)
-
           let teamAccess = projectBody.access.filter(access => access.type === 'team_access' )
           let teams = teamAccess[0].roles
-          console.log('!!teams', Array.isArray(teams), teams, teams[0], typeof teams[0] )
-          console.log('!!userteams', Array.isArray(userTeams), userTeams, userTeams[0], typeof userTeams[0] )
-
-          for (let team of userTeams) {
-            if (teams.includes(team)) {
-              console.log('SUCCESS')
-              return next()
-            }
-          }
           
           // Find intersection of userTeams and teams. 
-          // this intersect operation is broken... not finding common value
-          //let intersect = userTeams.filter(value => teams.includes(value))
+          let intersect = userTeams.filter(value => teams.includes(value))
   
           // If there are any overlapping roles, user can be authenticated
-          /*if(intersect.length > 0) {
+          if(intersect.length > 0) {
             return next()
-          }*/
+          }
           //This means if it fails on teams, it does not check roles
           return res.status(401).send('Unauthorized');
-
-
-
         });
       }
-      else {
+      else { // Not a superuser
         // Get roles with create access to the form
         request.get({
           url: `${req.query.baseUrl}/form/${req.query.form}`,
@@ -130,11 +115,6 @@ module.exports = function authenticate(req, res, next) {
         });
       }
     });
-
-    //1 issue: if form has no granted roles, it still should be able to be accessed by the superuser. Not sure how to tell
-    //  if a user is a superuser for the particular project. Worried that this solution will mean the "use" tab won't give you
-    //  access if no role is assigned to create
-
   }
   else {
     // Everything else is unauthorized.
